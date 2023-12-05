b647004eea2149d2a5fd713dd9418dfd37e7c739
Daniel Albornoz <danielalbornozmunoz@gmail.com>
2023-12-05 20:01:30 -0300

    sweet alert

--- static/js/mensaje_contacto.js
+++ static/js/mensaje_contacto.js
@@ -1,2 +1,14 @@
 // Mostrar el mensaje de contacto
-alert('¡Tu mensaje fue enviado correctamente!');
+//alert('¡Tu mensaje fue enviado correctamente!');
+
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
+
+function mensaje(){
+    Swal.fire({
+        title: 'Error!',
+        text: '¡Tu mensaje fue enviado correctamente!',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+      })          
+}
